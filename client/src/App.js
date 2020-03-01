1a1b8ec9c346557dc7e6f9001ee2ef2a3f910c4b
Ruben Chavez <chruben94@gmail.com>
2020-03-01 04:15:08 -0800

    3 out of 4 crud working

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
-import Books from "./pages/Books";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Appointments from "./pages/Appointments";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 
 function App() {
   return (
-    <div>
-      <Nav />
-      <Books />
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Appointments} />
+          <Route exact path="/appointments" component={Appointments} />
+          <Route exact path="/appointments/:id" component={Detail} />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
