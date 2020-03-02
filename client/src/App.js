f19c00c287ad57006aba8f7e63b86a57063aaa86
Ruben Chavez <chruben94@gmail.com>
2020-03-02 02:29:12 -0800

    made the pages

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,28 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Appointments from "./pages/Appointments";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
+import AllNav from "./components/Nav";
+import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <Router>
       <div>
-        <Nav />
+        <AllNav />
         <Switch>
           <Route exact path="/" component={Appointments} />
           <Route exact path="/appointments" component={Appointments} />
           <Route exact path="/appointments/:id" component={Detail} />
+          <Route exact path="/AboutUs" component={AboutUs} />
+
+          {/* <Route exact path="/" component={Home} />
+          
+          <Route exact path="/Schedule" component={Schedule} />
+          <Route exact path="/Instructors" component={Instructors} />
+          <Route exact path="/Locations" component={Locatiom} />
+          <Route exact path="/appointments" component={Appointments} />
+          <Route exact path="/appointments/:id" component={Detail} /> */}
+
           <Route component={NoMatch} />
         </Switch>
       </div>
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,13 +1,67 @@
-import React from "react";
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
 
-function Nav() {
+const AllNav = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-info">
-      <a className="navbar-brand" href="/">
-        NJ Swimming Group
-      </a>
-    </nav>
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">NJSG</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/AboutUs">About Us</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/Schedule">Schedule</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/Instructors">Instructors</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/Locations">Locations</NavLink>
+            </NavItem>
+            {/* <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                  Option 1
+                </DropdownItem>
+                <DropdownItem>
+                  Option 2
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  Reset
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown> */}
+          </Nav>
+          <NavbarText> <a href="#">Sign In</a></NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
   );
 }
 
-export default Nav;
+export default AllNav;
+
